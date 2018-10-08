9d2266e76b33ae7d4fd67e00e1745e522cd3bfb8
Kevin <Kevin@DESKTOP-12A507O>
2018-10-08 19:22:09 +0100

    Player fire and movement changed

---
+++ Assets/_scripts/PlayerBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+    float speed;
+    private void Start()
+    {
+        speed = 8f;
+    }
+    private void Update()
+    {
+        Vector2 position = transform.position;
+        position = new Vector2(position.x, position.y + speed * Time.deltaTime );
+
+        transform.position = position;
+
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+
+        if (transform.position.y > max.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/_scripts/PlayerController.cs
+++ Assets/_scripts/PlayerController.cs
@@ -1,27 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public GameObject bullet;
-    Vector2 bulletPos;
-    public float fireRate = 0.5f;
-    float nextFire = 0f;
+    public GameObject PlayerBullet;
+    public GameObject BulletPosition;
+    public float speed = 1;
     // Use this for initialization
     void Start () {
   
     }
-    public float speed = 1;
+    
     // Update is called once per frame
     void Update () {
-        var move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical"),0);
-        transform.position += move * speed * Time.deltaTime;
-        if(Input.GetButtonDown("Jump") && Time.time > nextFire)
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(x, y).normalized;
+        Move(direction);
+        if (Input.GetKeyDown("space"))
         {
-            nextFire = Time.time + fireRate;
-            fire();
+            GameObject bullet = (GameObject)Instantiate(PlayerBullet);
+            bullet.transform.position = BulletPosition.transform.position;
         }
+
+       
+       
     }
+
+    private void Move(Vector2 direction)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        max.x = max.x - 1.5f;
+        min.x = min.x + 1.5f;
+
+        max.y = max.y - 3.5f;
+        min.y = min.y + 1.5f;
+
+        Vector2 position = transform.position;
+
+        position += direction * speed * Time.deltaTime;
+
+        position.x = Mathf.Clamp(position.x, min.x, max.x);
+        position.y = Mathf.Clamp(position.y, min.y, max.y);
+
+        transform.position = position;
+
+
+
+    }
+
     void OnCollisionEnter2D(Collision2D col)
     {
         //Check collision name
@@ -34,11 +63,5 @@ public class PlayerController : MonoBehaviour {
         
 
     }
-    void fire()
-    {
-        bulletPos = transform.position;
-        bulletPos += new Vector2(+1f, -0.43f);
-        Instantiate(bullet, bulletPos, Quaternion.identity);
-      
-    }
+    
 }
